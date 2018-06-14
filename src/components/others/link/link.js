db7b5be4aa7ec142856adeebfc22ed8d803a6266
yTakkar <www.shtakkar@gmail.com>
2018-06-14 17:07:42 +0530

    A change in link proptypes

--- src/components/others/link/link.js
+++ src/components/others/link/link.js
@@ -1,6 +1,6 @@
 import React, { Fragment, Component } from 'react'
 import { Link } from 'react-router-dom'
-import { string, oneOfType, func } from 'prop-types'
+import { string } from 'prop-types'
 
 class AppLink extends Component {
   render() {
@@ -25,10 +25,7 @@ AppLink.defaultProps = {
 }
 
 AppLink.propTypes = {
-  label: oneOfType([
-    string,
-    func
-  ]).isRequired,
+  label: string,
   url: string.isRequired,
 }
 
